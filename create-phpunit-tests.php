dbf93d24665b576f3ca03425f3e4c1aa7f838437
irpsv <i.rpsv@live.com>
2017-07-02 20:54:00 +0500

    Update create-phpunit-tests.php

--- create-phpunit-tests.php
+++ create-phpunit-tests.php
@@ -16,12 +16,85 @@ $phpunit = trim("
 ");
 file_put_contents("$dir/phpunit.xml", $phpunit);
 
-if (!mkdir("$dir/tests")) {
-	exit("Can't create dir 'tests'");
+if (!mkdir("$dir/test")) {
+	exit("Can't create dir 'test'");
 }
 
-$bootstrap = "<?php\n\n";
-file_put_contents("$dir/tests/bootstrap.php", $bootstrap);
+$bootstrap = trim("
+<?php
+
+class Psr4AutoloaderClass
+{
+    protected \$prefixes = array();
+
+    public function register()
+    {
+        spl_autoload_register(array(\$this, 'loadClass'));
+    }
+
+    public function addNamespace(\$prefix, \$base_dir, \$prepend = false)
+    {
+        \$prefix = trim(\$prefix, '\\') . '\\';
+        \$base_dir = rtrim(\$base_dir, DIRECTORY_SEPARATOR) . '/';
+        if (isset(\$this->prefixes[\$prefix]) === false) {
+            \$this->prefixes[\$prefix] = array();
+        }
+		
+        if (\$prepend) {
+            array_unshift(\$this->prefixes[\$prefix], \$base_dir);
+        } else {
+            array_push(\$this->prefixes[\$prefix], \$base_dir);
+        }
+    }
+
+    public function loadClass(\$class)
+    {
+        \$prefix = \$class;
+        while (false !== \$pos = strrpos(\$prefix, '\\')) {
+            \$prefix = substr(\$class, 0, \$pos + 1);
+            \$relative_class = substr(\$class, \$pos + 1);
+            \$mapped_file = \$this->loadMappedFile(\$prefix, \$relative_class);
+            if (\$mapped_file) {
+                return \$mapped_file;
+            }
+            \$prefix = rtrim(\$prefix, '\\');
+        }
+        return false;
+    }
+
+    protected function loadMappedFile(\$prefix, \$relative_class)
+    {
+        if (isset(\$this->prefixes[\$prefix]) === false) {
+            return false;
+        }
+        foreach (\$this->prefixes[\$prefix] as \$base_dir) {
+
+            \$file = \$base_dir
+                  . str_replace('\\', '/', \$relative_class)
+                  . '.php';
+            if (\$this->requireFile(\$file)) {
+                return \$file;
+            }
+        }
+        return false;
+    }
+
+    protected function requireFile(\$file)
+    {
+        if (file_exists(\$file)) {
+            require \$file;
+            return true;
+        }
+        return false;
+    }
+}
+
+\$loader = new Psr4AutoloaderClass();
+\$loader->addNamespace('test', __DIR__.'/../test');
+\$loader->addNamespace('your_namescape', __DIR__.'/../src');
+
+");
+file_put_contents("$dir/test/bootstrap.php", $bootstrap);
 
 $general = trim("
 <?php
@@ -36,6 +109,6 @@ class GeneralTest extends TestCase
     }
 }
 ");
-file_put_contents("$dir/tests/general.php", $general);
+file_put_contents("$dir/test/general.php", $general);
 
 exit("Done");
